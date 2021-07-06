5126cf4dd1b36a5fb2d9c4096ffb44ee26959384
Levhita Arias <levhita@gmail.com>
2021-07-06 00:08:20 -0500

    New Crops

--- World/Props/Crops/Crops.gd
+++ World/Props/Crops/Crops.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+export(String, "Wheat", "Rice") var crop = "Wheat"
 onready var animatedSprite = $AnimatedSprite
 
+func _ready():
+	var cropToLoad = "res://World/Props/Crops/" + crop + ".tres"
+	var cropSpriteFrames = load(cropToLoad)
+	animatedSprite.frames = cropSpriteFrames
+	
 func destroy():
 	animatedSprite.play("cut")
 	pass
@@ -9,7 +15,6 @@ func destroy():
 func _on_Hurtbox_area_entered(area):
 	destroy()
 	
-
 
 func _on_Sprite_animation_finished():
 	queue_free()
